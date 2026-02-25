7382c1df03a47afcbe6e0494127f8b14482d1284
Julian <q:wq>
2026-02-25 18:18:02 +0100

    add git ignore for content for dev branch

--- habit-tracker/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // WICHTIG: Erlaubt das Hosten auf GitHub Pages in Unterverzeichnissen
-  base: './', 
-})
